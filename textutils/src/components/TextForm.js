4076b542b03c31d5689b3faf2a51bc7a5574f5f3
vishalpande <pandevishal40@gmail.com>
2023-03-22 15:34:16 +0530

    Updating the app

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
 
+const handleUpClick2=()=>{
+const newText2=text.toLowerCase();
+setText(newText2)
+
+}
+
 const handleUpClick=()=>{
 // console.log("Uppercase was clicked " + text );
 const newText=text.toUpperCase();
@@ -25,7 +31,10 @@ const [text,setText]=useState('Enter text here');
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} placeholderid="myBox" rows="8"></textarea>
 </div>
+<div class="d-grid gap-2 d-md-flex ">
 <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+<button className="btn btn-secondary " onClick={handleUpClick2}>Convert to lowercase</button>
+</div>
 </>
 
   )
