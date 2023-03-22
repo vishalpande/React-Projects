9afb4689a7fd090317e78e26eff204dda23ab112
vishalpande <pandevishal40@gmail.com>
2023-03-22 14:02:55 +0530

    Updating

---
+++ textutils/src/components/TextForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+
+<div className="mb-3">
+  <label for="myBox" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="myBox" rows="3"></textarea>
+</div>
+
+
+  )
+}
--- textutils/src/index.js
+++ textutils/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TextForm from './components/TextForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <TextForm/>
   </React.StrictMode>
 );
 
