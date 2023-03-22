830695f18bea051f9a7fb8325555646d38bc195a
vishalpande <pandevishal40@gmail.com>
2023-03-22 14:58:06 +0530

    Updating

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
 <>
 <Navbar title={"TextUtils"} aboutTitle={'About'} />
+<div className="container ">
+<TextForm heading='Enter the text to analyze below'/>
+</div>
+
+
 
 </>
   );
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
+
+const handleUpClick=()=>{
+console.log("Uppercase was clicked " + text );
+const newText=text.toUpperCase();
+setText(newText);
+
+}
+const handleOnChange=(event)=>{
+console.log("OnChange was clicked");
+setText(event.target.value)
+
+
+
+}
+
+const [text,setText]=useState('Enter text here');
+// setText('hello') ;
+
   return (
-
+ <>
+ <h1>{props.heading}</h1>
 <div className="mb-3">
-  <label for="myBox" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="myBox" rows="3"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} placeholderid="myBox" rows="8"></textarea>
 </div>
-
+<button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+</>
 
   )
 }
